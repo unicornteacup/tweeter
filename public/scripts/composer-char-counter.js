0270b266aa9f7bb6b068b61874d0494c75a18d85
Caroline <carolinejepps@gmail.com>
2020-07-14 22:40:53 +0000

    Added counter jQuery w/ link to index.html

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  
+  $('#tweet-text').mouseover(function() {
+    console.log('test');
+  }); 
+
+  $('#tweet-text').keypress(function() {
+    const tweetChars = $(this).val().length
+    console.log(tweetChars);
+    if (tweetChars > 140) {
+      $('output').css('color', 'red');
+    }
+    $('output').val(140 - tweetChars);
+
+  }) 
+
+});
